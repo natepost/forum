aad940601e21a634f995c7c14721a5249f9407e7
natepost <nathanielpost@gmail.com>
2017-09-29 18:27:37 -0400

    Basic CRUD setup for Experiments

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,18 +1,50 @@
 class ExperimentsController < ApplicationController
+  before_action :find_experiment, only: [:show, :edit, :update, :destroy]
+
   def index
+    @experiments = Experiment.all
   end
 
   def new
     @experiment = Experiment.new
   end
 
+  def show
+  end
+
   def create
-    @experiment = Experiment.new(post_params)
+    @experiment = Experiment.new(experiment_params)
+
+    if @experiment.save
+      redirect_to @experiment
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @experiment.update(experiment_params)
+      redirect_to @experiment
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @experiment.destroy
+    redirect_to root_path
   end
 
   private
 
-  def post_params
+  def find_experiment
+    @experiment = Experiment.find(params[:id])
+  end
+
+  def experiment_params
     params.require(:experiment).permit(:title, :content)
   end
 end
