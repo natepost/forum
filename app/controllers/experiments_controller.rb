71eabc19323191c080e30d153afd0d952e765822
natepost <nathanielpost@gmail.com>
2017-09-29 15:41:42 -0400

    Add new experiment form

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,4 +1,18 @@
 class ExperimentsController < ApplicationController
   def index
   end
+
+  def new
+    @experiment = Experiment.new
+  end
+
+  def create
+    @experiment = Experiment.new(post_params)
+  end
+
+  private
+
+  def post_params
+    params.require(:experiment).permit(:title, :content)
+  end
 end
